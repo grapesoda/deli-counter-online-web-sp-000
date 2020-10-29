29f4a8a0c44d267d9719d652ffa1e16bc3aad2c7
Tres Tarwater <trest03@gmail.com>
2020-10-29 19:33:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,15 +2,15 @@
 
 
 
-def line(x)
-  line_array = []
-  if x.length == 0
+def line(katz_deli)
+  if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    x.each.with_index(1) do |name, index|
-      line_array.push("#{index}. #{name}")
+    current_line = "The line is currently:"
+    katz_deli.each.with_index(1) do |person, index|
+      current_line << " #{index}. #{person}"
     end
-    puts "The line is currently: #{line_array.join(" ")}"
+    puts current_line
   end
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
