84fd5ec311d414887559a269cbfea1323984cd6c
Tres Tarwater <trest03@gmail.com>
2020-10-29 19:28:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    katz_deli.each.with_index(1) do |person, index|
+      current_line << " #{index}. #{person}"
+    end
+    puts current_line
+  end
+end
